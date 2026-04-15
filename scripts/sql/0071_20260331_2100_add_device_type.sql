ae77f911e49611925f5988153e5fafcbecdb787d
connorzheng <connorzheng@tencent.com>
2026-04-15 02:40:12 +0000

    feat: v1.8.11.1 --story=132843835 (merge request !3010)
    Squash merge branch 'v1.8.11.1' into 'bcc/v1.8.x'
    feat: v1.8.11.1 --story=132843835
    
    
    TAPD: --story=132843835

--- scripts/sql/0071_20260331_2100_add_device_type.sql
+++ scripts/sql/0071_20260331_2100_add_device_type.sql
@@ -18,7 +18,7 @@
  */
 
 /*
-    SQLVER=9999,HCMVER=v9.9.9
+    SQLVER=0071,HCMVER=v1.8.11.1
 
     Notes:
     1. 机型表添加`机型代次`字段
@@ -30,6 +30,6 @@ alter table device_type
     add column `generation_type` varchar(64) not null default '' comment '机型代次' after `source`;
 
 CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
-SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+SELECT 'v1.8.11.1' as `hcm_ver`, '0071' as `sql_ver`;
 
 COMMIT;
