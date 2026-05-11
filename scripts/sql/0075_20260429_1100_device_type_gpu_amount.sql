27d18ff7f74ac7ca6bebc962ec35f2ccda4745a1
pandafyang <pandafyang@tencent.com>
2026-05-11 12:49:58 +0000

    docs: v1.8.11.12 (merge request !3065)
    Squash merge branch 'version/v1.8.11.12' into 'bcc/v1.8.x'
    docs: v1.8.11.12
    
    
    TAPD: --story=133842366

--- scripts/sql/0075_20260429_1100_device_type_gpu_amount.sql
+++ scripts/sql/0075_20260429_1100_device_type_gpu_amount.sql
@@ -18,7 +18,7 @@
  */
 
 /*
-    SQLVER=9999,HCMVER=v9.9.9
+    SQLVER=0075,HCMVER=v1.8.11.12
 
     Notes:
     1. 机型表添加 gpu_amount 字段
@@ -30,6 +30,6 @@ ALTER TABLE `device_type`
     ADD COLUMN `gpu_amount` DOUBLE NOT NULL DEFAULT 0 COMMENT 'GPU卡数' AFTER `memory`;
 
 CREATE OR REPLACE VIEW `hcm_version`(`hcm_ver`, `sql_ver`) AS
-SELECT 'v9.9.9' as `hcm_ver`, '9999' as `sql_ver`;
+SELECT 'v1.8.11.12' as `hcm_ver`, '0075' as `sql_ver`;
 
 COMMIT;
